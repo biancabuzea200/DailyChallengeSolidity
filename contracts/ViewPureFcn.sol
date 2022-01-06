fb591ff9e1b7187feb80d4ca77bcaa55b6786c3f
Bianca Buzea <biancabuzea200@gmail.com>
2022-01-06 17:49:08 +0100

    view and pure fcns example

---
+++ contracts/ViewPureFcn.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract ViewAndPure {
+   uint public x = 1;
+
+   // view: no stste will be changed
+   // pure: no state variable will be changed or read
+
+   function addToX(uint y) public view returns (uint){
+       return x + y;
+   }
+
+   function add (uint a, uint b) public pure returns (uint){
+       return a + b;
+   }
+}
