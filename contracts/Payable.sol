6d0cdc9f8cf3eb73e9e1a50b89722d4f859ec65b
Bianca Buzea <biancabuzea200@gmail.com>
2022-01-21 23:58:44 +0100

    payable.sol add comments

--- contracts/Payable.sol
+++ contracts/Payable.sol
@@ -1,15 +1,33 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.0;
 
 contract Payable {
     address payable public owner;
 
+    //payable constructor can receive ether
     constructor() payable {
         owner = payable (msg.sender);
     }
 
+    //function to deposit ether into the contract, call it along with some ether
+    //balance will autom. be updated
     function deposit() public payable {}
 
+    //call this fcn along with some ether
+    //fcn will throw since not payable
     function notPayable() public {}
+
+    function withdraw() public {
+        uint amount = address(this).balance;
+        (bool success, ) = owner.call {value: amount}("");
+        require(success, "failed to send ether");
+    }
+
+    //_to is declatred as payable
+
+    function transfer (address payable _to, uint _amount) public {
+        (bool success , ) =_to.call{value: _amount}("");
+        require(success, "failed to send ether");
+    }
   
 }
