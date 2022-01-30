4435301d05ebd926957d5b58bc46073aa415280e
Bianca Buzea <biancabuzea200@gmail.com>
2022-01-30 23:58:10 +0100

    Import.sol

---
+++ contracts/Import.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+struct Point {
+    uint x;
+    uint y;
+}
+
+error Unauthorized(address caller);
+
+function add (uint x, uint y) pure returns (uint) {
+    return x + y;
+}
+
+contract foo {
+    string public name = "Foo";
+}
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./Foo.sol";
+
+import {Unauthorized, add as func, Point} from "./Foo.sol";
+
+contract Import {
+    Foo public foo = new Foo ();
+
+    function getFooName() public view returns (string memory) {
+        return foo.name();
+    }
+}
+
+
+import "https://github.com/owner/repo/blob/branch/path/to/Contract.sol";
+
+// Example import ECDSA.sol from openzeppelin-contract repo, release-v3.3 branch
+// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.3/contracts/cryptography/ECDSA.sol
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.3/contracts/cryptography/ECDSA.sol";
+
